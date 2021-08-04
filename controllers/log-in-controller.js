5c128bea8735c1531e2edb4720dc87f8bc0f5f12
Hylene Lee <hylene_jules_lee@dlsu.edu.ph>
2021-08-04 21:18:55 +0800

    Update controller for business owner log in

--- controllers/log-in-controller.js
+++ controllers/log-in-controller.js
@@ -13,92 +13,84 @@ const logInController = {
 		let queryUsername = {username: username};
         let queryEmail = {emailAddress: username};
 
-        db.findOne(Client, queryUsername, '', function (result) {
-            if (result) {
-                let client = {
-                    username: result.username,
-                    firstName: result.firstName,
-                    middleName: result.middleName,
-                    lastName: result.lastName,
-                    emailAddress: result.emailAddress,
-                    contactNumber: result.contactNumber,
-                    deliveryAddress: result.deliveryAddress,
-                    pictureFileName: result.pictureFileName
-                }
+        let queryAdmin = {username: "kraftsbykatadmin"};
 
-                bcrypt.compare(password, result.password, function (err, equal) {
-                    if (equal) {
-                        res.status(200).send();
-                    } else {
-                        res.status(403).send();
+        /* Log in for admin account */
+        if (JSON.stringify(username) == JSON.stringify("kraftsbykatadmin") || 
+            JSON.stringify(username) == JSON.stringify("krafts.by.kat.webmaster@gmail.com")) {
+            db.findOne(BusinessOwner, queryAdmin, '', function (result) {
+                if (result) {
+                    let businessOwner = {
+                        username: result.username,
+                        emailAddress: result.emailAddress,
+                        contactNumber: result.contactNumber,
+                        deliveryAddress: result.deliveryAddress,
+                        pictureFileName: result.pictureFileName
                     }
-                });
-                
-            } else {
-                db.findOne(Client, queryEmail, '', function (result) {
-                    if (result) {
-                        let client = {
-                            username: result.username,
-                            firstName: result.firstName,
-                            middleName: result.middleName,
-                            lastName: result.lastName,
-                            emailAddress: result.emailAddress,
-                            contactNumber: result.contactNumber,
-                            deliveryAddress: result.deliveryAddress,
-                            pictureFileName: result.pictureFileName
+
+                    bcrypt.compare(password, result.password, function (err, equal) {
+                        if (equal) {
+                            res.status(200).send();
+                        } else {
+                            res.status(403).send();
                         }
+                    });
+                } else {
+                    res.status(403).send();
+                }
+            });
 
-                        bcrypt.compare(password, result.password, function (err, equal) {
-                            if (equal) {
-                                res.status(200).send();
-                            } else {
-                                res.status(403).send();
+        /* Log in for client accounts */
+        } else {
+            db.findOne(Client, queryUsername, '', function (result) {
+                if (result) {
+                    let client = {
+                        username: result.username,
+                        firstName: result.firstName,
+                        middleName: result.middleName,
+                        lastName: result.lastName,
+                        emailAddress: result.emailAddress,
+                        contactNumber: result.contactNumber,
+                        deliveryAddress: result.deliveryAddress,
+                        pictureFileName: result.pictureFileName
+                    }
+
+                    bcrypt.compare(password, result.password, function (err, equal) {
+                        if (equal) {
+                            res.status(200).send();
+                        } else {
+                            res.status(403).send();
+                        }
+                    });
+                    
+                } else {
+                    db.findOne(Client, queryEmail, '', function (result) {
+                        if (result) {
+                            let client = {
+                                username: result.username,
+                                firstName: result.firstName,
+                                middleName: result.middleName,
+                                lastName: result.lastName,
+                                emailAddress: result.emailAddress,
+                                contactNumber: result.contactNumber,
+                                deliveryAddress: result.deliveryAddress,
+                                pictureFileName: result.pictureFileName
                             }
-                        });
-                     } else {
-                        db.findOne(BusinessOwner, queryUsername, '', function (result) {
-                            if (result) {
-                                let businessOwner = {
-                                    username: result.username,                              
-                                    emailAddress: result.emailAddress,
-                                    contactNumber: result.contactNumber,
-                                    pictureFileName: result.pictureFileName
+
+                            bcrypt.compare(password, result.password, function (err, equal) {
+                                if (equal) {
+                                    res.status(200).send();
+                                } else {
+                                    res.status(403).send();
                                 }
-
-                                bcrypt.compare(password, result.password, function (err, equal) {
-                                    if (equal) {
-                                        res.status(200).send();
-                                    } else {
-                                        res.status(403).send();
-                                    }
-                                });
-                             } else {
-                                db.findOne(BusinessOwner, queryEmail, '', function (result) {
-                                    if (result) {
-                                        let businessOwner = {
-                                            username: result.username,                              
-                                            emailAddress: result.emailAddress,
-                                            contactNumber: result.contactNumber,
-                                            pictureFileName: result.pictureFileName
-                                        }
-
-                                        bcrypt.compare(password, result.password, function (err, equal) {
-                                            if (equal) {
-                                                res.status(200).send();
-                                            } else {
-                                                res.status(403).send();
-                                            }
-                                        });
-                                     } else {
-                                        res.status(403).send();
-                                     }  
-                                });
-                             }  
-                        });
-                     }  
-                });
-            }
-        });
+                            });
+                         } else {
+                            res.status(403).send();
+                         }  
+                    });
+                }
+            });
+        }
     }
 }
 
