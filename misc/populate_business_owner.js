757a9d1b53a0c48cf136d256c3e8a4d6c07adb8f
Hylene Lee <hylene_jules_lee@dlsu.edu.ph>
2021-08-15 23:07:44 +0800

    Add comments for files in misc folder

--- misc/populate_business_owner.js
+++ misc/populate_business_owner.js
@@ -1,12 +1,24 @@
+/* Script for populating the database with an administrator account to be used by the business owner */
+
+/* Mongoose is used for database functions */
 const mongoose = require('mongoose');
+
+/* The db file and business owner schema are used to manipulate the administrator account on the database */
 const db = require('../models/db.js');
 const BusinessOwner = require('../models/business-owner-schema.js');
+
+/* Bcrypt is used for password hashing */
 const bcrypt = require('bcrypt');
+
+/* Ten salt rounds are used for password hashing */
 const saltRounds = 10;
 
+/* Establish a connection to the database */
 db.connect();
 
+/* Add an administrator account to the database with the password ASDFGHJKL123; */
 bcrypt.hash("ASDFGHJKL123;", saltRounds, function (err, hash) {
+	/* Initialize administrator account details */
 	let businessOwner1 = {
 		username: "kraftsbykatadmin",
 		password: hash,
@@ -16,7 +28,8 @@ bcrypt.hash("ASDFGHJKL123;", saltRounds, function (err, hash) {
 		picture: ""
 	};
 
-	db.insertMany(BusinessOwner, [businessOwner1], function(flag) {	
+	/* Insert initialized administrator account into the database */
+	db.insertOne(BusinessOwner, businessOwner1, function(flag) {	
 		if (flag) {
 			console.log("\nDatabase population complete! Press Ctrl + C to continue.");
 		}
--- misc/populate_display.js
+++ misc/populate_display.js
@@ -1,14 +1,22 @@
+/* Script for populating the database with a dummy logo */
+
+/* Mongoose is used for database functions */
 const mongoose = require('mongoose');
+
+/* The db file and display schema are used to manipulate the logos on the database */
 const db = require('../models/db.js');
 const Display = require('../models/display-schema.js');
 
+/* Establish a connection to the database */
 db.connect();
 
+/* Initialize a dummy logo */
 let display1 = {
 	id: 0,
 	logo: "lorem_ipsum"
 };
 
+/* Insert initialized logo into the database */
 db.insertOne(Display, display1, function(flag) {
 	if (flag) {
 		console.log("\nDisplay database population complete! Press Ctrl + C to continue.");
