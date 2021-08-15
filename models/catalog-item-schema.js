d8dff22d04cba0ab5ca13630bb4cce339ad2dc29
Hylene Lee <hylene_jules_lee@dlsu.edu.ph>
2021-08-15 22:55:13 +0800

    Add comments for files in models folder

--- models/catalog-item-schema.js
+++ models/catalog-item-schema.js
@@ -1,41 +1,50 @@
 const mongoose = require('mongoose');
 
+/* Schema for the catalog items */
 const catalogItemSchema = new mongoose.Schema({
+    /* Item name */
     name: {
         type: String,
         required: true
     },
 	
+    /* Item quantity */
 	quantity: {
 		type: Number,
 		required: true
 	},
 
+    /* Item description (optional) */
     description: {
         type: String,
         required: false
     },
 
+    /* Item price */
     price: {
         type: Number,
         required: true
     },
 
+    /* ObjectIDs of customer comments regarding the item */
     commentIds: {
         type: [Number],
         required: false
     },
 
+    /* Images of the item (optional) */
     pictures: {
         type: [String],
         required: false
     },
 
+    /* Customer ratings of the item */
     ratings: {
         type: [Number],
         required: false
     },
 
+    /* Number of units sold of the item */
     numberSold: {
         type: Number,
         required: true
