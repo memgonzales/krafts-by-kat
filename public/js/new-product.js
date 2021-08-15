023aab83dce86a9f7a826ccf3d2a082a4dd64dcd
Mark Edward M. Gonzales <gonzales.markedward@gmail.com>
2021-08-15 11:36:52 +0800

    Rewrite onchange to jQuery

--- public/js/new-product.js
+++ public/js/new-product.js
@@ -39,23 +39,31 @@ $(document).ready(function() {
 	$('#pic-5').on('click', function() {
 		$('#product-img-5').click();
 	});
-});
 
-let imgTargetResults = [];
-
-function readURL(input, i) {
-	if (input.files && input.files[0]) {
-		let reader = new FileReader();
-		reader.onload = function(e) {
-			$('#img-' + i).css('display', 'none');
-			$('#pic-' + i).attr('src', e.target.result).width(150).height(100);
-
-			/* Store the image file paths in an array */
-			imgTargetResults.push(e.target.result);
-		}
-		
-		reader.readAsDataURL(input.files[0]);
+	/* Change the picture upon upload */
+	for (let i = 1; i <= 5; i++) {
+		$('#product-img-' + i).on('change', function() {
+			readURL(this, i);
+		});
 	}
 
-	alert(imgTargetResults);
-}
+	let imgTargetResults = [];
+
+	function readURL(input, i) {
+		if (input.files && input.files[0]) {
+			let reader = new FileReader();
+			reader.onload = function(e) {
+				$('#img-' + i).css('display', 'none');
+				$('#pic-' + i).attr('src', e.target.result).width(150).height(100);
+
+				/* Store the image file paths in an array */
+				imgTargetResults.push(e.target.result);
+			}
+			
+			reader.readAsDataURL(input.files[0]);
+		}
+
+		alert(imgTargetResults);
+	}
+});
+
