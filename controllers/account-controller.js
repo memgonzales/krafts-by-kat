8af683177a64d641c5c24080d1d30f4272e62bfc
Hylene Lee <hylene_jules_lee@dlsu.edu.ph>
2021-08-17 23:42:47 +0800

    Implement products manager page

--- controllers/account-controller.js
+++ controllers/account-controller.js
@@ -1,9 +1,10 @@
 /* Controller for handling the account page */
 
-/* The db file, display schema, and client schema are used for the account page */
+/* The db file, display schema, client schema, and catalog schema are used for the account page */
 const db = require('../models/db.js');
 const Display = require('../models/display-schema.js');
 const Client = require('../models/client-schema.js');
+const CatalogItem = require('../models/catalog-item-schema.js');
 
 const accountController = {
 	
@@ -275,24 +276,193 @@ const accountController = {
 			
 			/* If the data retrieval was successful, display the account page */
 			if (result) {
+				/* Assign the result of the database retrieval to the variable appLogo */
+				let appLogo = result;
 
-				/* If the user is using an administrator account, display the nav bar accordingly */
+				/* If the user is using an administrator account, display the page accordingly */
 				if (req.session.isAdmin == true) {
-					let details = {
-						style: 'account',
-						logo: result.logo,
-						userFlag: true,
-						adminFlag: true,
-						username: req.session.username,
-						isAdmin: req.session.isAdmin
-					}
+					
+					/* Assign the needed details of the item catalog documents to the variable projection */
+					let projection = '_id name quantity description price commentIds pictures ratings numberSold visible';
 
-					res.render('admin-products-manager', details);
+					/* The needed details of all catalog items are retrieved to be displayed on the landing page */
+					db.findMany(CatalogItem, {}, projection, function(result) {
+						/* If the data retrieval is successful, the landing page is displayed */
+						if (result != null) {
+							/* Store data in parallel arrays (to allow for processing); the first set of arrays
+							 * contains the data of active items (i.e., those that can be purchased by customers) 
+							 */
+							let activeIds = [];
+							let activeNames = [];
+							let activeQuantities = [];
+							let activeDescriptions = [];
+							let activePrices = [];
+							let activeComments = [];
+							let activePictures = [];
+							let activeRatings = [];
+							let activeAveRatings = [];
+							let activeNumberSold = [];
 
+							/* The second set of arrays contains the data of hidden items */
+							let hiddenIds = [];
+							let hiddenNames = [];
+							let hiddenQuantities = [];
+							let hiddenDescriptions = [];
+							let hiddenPrices = [];
+							let hiddenComments = [];
+							let hiddenPictures = [];
+							let hiddenRatings = [];
+							let hiddenAveRatings = [];
+							let hiddenNumberSold = [];
+
+							/* The third set of arrays contains the data of depleted items 
+							 * (i.e., those that are no longer in stock)
+							 */
+							let depletedIds = [];
+							let depletedNames = [];
+							let depletedQuantities = [];
+							let depletedDescriptions = [];
+							let depletedPrices = [];
+							let depletedComments = [];
+							let depletedPictures = [];
+							let depletedRatings = [];
+							let depletedAveRatings = [];
+							let depletedNumberSold = [];
+
+							/* Assign the results of the database retrieval to the variable items */
+							let items = result;
+							/* For each catalog item, push each detail to its respective array if the item
+							 * has been made visible and is not depleted 
+							 */
+							for (let i = 0; i < items.length; i++) {
+								if (items[i].visible == false) {
+									hiddenIds.push(items[i]._id);
+									hiddenNames.push(items[i].name);
+									hiddenQuantities.push(items[i].quantity);
+									hiddenDescriptions.push(items[i].description);
+									hiddenPrices.push(items[i].price);
+									hiddenNumberSold.push(items[i].numberSold);
+									/* Currently, commentIds are stored as comments (will change to actual comments once comment functionality has been implemented) */
+									hiddenComments.push(items[i].commentIds);
+									hiddenPictures.push(items[i].pictures[0]);
+									hiddenRatings.push(items[i].ratings);
+
+									/* Average rating is displayed as 0 if there are no ratings yet */
+									if (items[i].ratings.length == 0) {
+										hiddenAveRatings.push(0);
+
+									/* Otherwise, the average rating is computed and displayed */
+									} else {
+										var totalRating = 0;
+
+										for (let j = 0; j < items[i].ratings.length; j++) {
+											totalRating += items[i].ratings[j];
+										}
+
+										hiddenAveRatings.push(totalRating / items[i].ratings.length);
+									}
+								} else if (items[i].quantity <= 0) {
+									depletedIds.push(items[i]._id);
+									depletedNames.push(items[i].name);
+									depletedQuantities.push(items[i].quantity);
+									depletedDescriptions.push(items[i].description);
+									depletedPrices.push(items[i].price);
+									depletedNumberSold.push(items[i].numberSold);
+									/* Currently, commentIds are stored as comments (will change to actual comments once comment functionality has been implemented) */
+									depletedComments.push(items[i].commentIds);
+									depletedPictures.push(items[i].pictures[0]);
+									depletedRatings.push(items[i].ratings);
+
+									/* Average rating is displayed as 0 if there are no ratings yet */
+									if (items[i].ratings.length == 0) {
+										depletedAveRatings.push(0);
+
+									/* Otherwise, the average rating is computed and displayed */
+									} else {
+										var totalRating = 0;
+
+										for (let j = 0; j < items[i].ratings.length; j++) {
+											totalRating += items[i].ratings[j];
+										}
+
+										depletedAveRatings.push(totalRating / items[i].ratings.length);
+									}
+								} else {
+									activeIds.push(items[i]._id);
+									activeNames.push(items[i].name);
+									activeQuantities.push(items[i].quantity);
+									activeDescriptions.push(items[i].description);
+									activePrices.push(items[i].price);
+									activeNumberSold.push(items[i].numberSold);
+									/* Currently, commentIds are stored as comments (will change to actual comments once comment functionality has been implemented) */
+									activeComments.push(items[i].commentIds);
+									activePictures.push(items[i].pictures[0]);
+									activeRatings.push(items[i].ratings);
+
+									/* Average rating is displayed as 0 if there are no ratings yet */
+									if (items[i].ratings.length == 0) {
+										activeAveRatings.push(0);
+
+									/* Otherwise, the average rating is computed and displayed */
+									} else {
+										var totalRating = 0;
+
+										for (let j = 0; j < items[i].ratings.length; j++) {
+											totalRating += items[i].ratings[j];
+										}
+
+										activeAveRatings.push(totalRating / items[i].ratings.length);
+									}
+								}
+							}
+
+							let details = {
+								style: 'account',
+								logo: appLogo,
+								userFlag: true,
+								adminFlag: true,
+								username: req.session.username,
+								isAdmin: req.session.isAdmin,
+
+								activeIds: activeIds,
+								activeNames: activeNames,
+								activeQuantities: activeQuantities,
+								activeDescriptions: activeDescriptions,
+								activePrices: activePrices,
+								activeComments: activeComments,
+								activePictures: activePictures,
+								activeAveRatings: activeAveRatings,
+								activeNumberSold: activeNumberSold,
+
+								hiddenIds: hiddenIds,
+								hiddenNames: hiddenNames,
+								hiddenQuantities: hiddenQuantities,
+								hiddenDescriptions: hiddenDescriptions,
+								hiddenPrices: hiddenPrices,
+								hiddenComments: hiddenComments,
+								hiddenPictures: hiddenPictures,
+								hiddenAveRatings: hiddenAveRatings,
+								hiddenNumberSold: hiddenNumberSold,
+
+								depletedIds: depletedIds,
+								depletedNames: depletedNames,
+								depletedQuantities: depletedQuantities,
+								depletedDescriptions: depletedDescriptions,
+								depletedPrices: depletedPrices,
+								depletedComments: depletedComments,
+								depletedPictures: depletedPictures,
+								depletedAveRatings: depletedAveRatings,
+								depletedNumberSold: depletedNumberSold
+							}
+
+							res.render('admin-products-manager', details);
+						}
+					});
+							
 				/* If the user is not the admin, redirect them to the landing page */
 				} else {
 					res.redirect('/');
-				}	
+				}
 
 			/* If the data retrieval was not successful, display an error message */			
 			} else {
