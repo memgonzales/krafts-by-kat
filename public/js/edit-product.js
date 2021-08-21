f7857d85abe9b571545bbf8f13f4bfa263196a0a
Mark Edward M. Gonzales <gonzales.markedward@gmail.com>
2021-08-22 01:07:08 +0800

    Handle display of product pictures in edit product page

---
+++ public/js/edit-product.js
@@ -0,0 +1,58 @@
+$(document).ready(function() {
+    formatNumber('#units-sold');
+    formatNumber('#units-available');
+    formatNumber('#product-price-num');
+
+    const pictures = getPictures();
+    displayPictures();
+
+    function formatNumber(id) {
+        let number = $(id).text();
+        let formatted = '';
+
+        if (number) {
+            if (id == '#product-price-num') {
+                formatted = number.substr(1);
+                formatted = parseFloat(formatted.trim()).toLocaleString('en-US', {maximumFractionDigits: 2});
+            } else {
+                formatted = parseFloat(number.trim()).toLocaleString('en-US', {maximumFractionDigits: 2});
+            }
+    
+        }
+
+        if (id == '#product-price-num') {
+            formatted = '₱' + formatted;
+        }
+
+        $(id).text(formatted);
+    }
+
+    function getPictures() {
+        const pathsString = $('#picturePaths').text();
+        const paths = pathsString.split(',');
+
+        return paths;
+    }
+
+    function displayPictures() {
+        const numPics = $('#small-view-pic-container').length;
+        const placeholder = '/img/placeholder/no-image.png';
+
+        /* Display pictures only if pictures have been uploaded */
+        if (pictures[0] != placeholder) {
+            /* Update the large picture, and set it to the first picture in the polaroid display */
+            $('#pic-big').css('display', 'block');
+            $('#pic-big').attr('src', pictures[0]);
+            $('#icon-big').css('display', 'none');    
+
+            /* Use one-based indexing to conform with the IDs of the HTML elements */
+            for (let i = 1; i <= pictures.length; i++) {
+                $('#pic' + i).css('display', 'inline-block');
+                $('#icon' + i).css('display', 'none');
+
+                /* Use zero-based indexing to conform with array notation */
+                $('#pic' + i).attr('src', pictures[i - 1]);
+            }
+        }
+    }
+});
