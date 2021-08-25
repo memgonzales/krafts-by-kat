941393f0251ff5d26a647fea4ceaa186421df85a
Mark Edward M. Gonzales <gonzales.markedward@gmail.com>
2021-08-26 00:31:02 +0800

    Enable image upload when editing product

--- public/js/edit-product.js
+++ public/js/edit-product.js
@@ -11,6 +11,10 @@ $(document).ready(function() {
     /* The maximum number of pictures is the number of children of the div with the id below. */
     const maxNumPictures = $('#small-view-pic-container').children().length;
 
+    /* Call the methods for uploading (editing) a picture */
+    triggerUpload();
+    changePicOnUpload();
+	
     /* 
 	 * Initialize an array to store the image file paths.
 	 * 
@@ -21,6 +25,17 @@ $(document).ready(function() {
 	for (let i = 0; i < maxNumPictures; i++) {
 		imgTargetResultsOrig.push('');
 	}
+
+    /*
+     * Initialize an array to store a Boolean value corresponding to whether a photo has been edited
+     *
+     * Each photo is identified via a zero-based index. Initially, all of them are unmodified; thus,
+     * the initial values are all set to false.
+     */
+    let modifiedIndices = [];
+    for (let i = 0; i < maxNumPictures; i++) {
+        modifiedIndices.push(false);
+    }
 
     /**
      * Formats the number given the ID of its HTML container so that commas are used to separate groups
@@ -95,13 +110,69 @@ $(document).ready(function() {
 	 */
 	function triggerUpload() {
 		for (let i = 1; i <= maxNumPictures; i++) {
-			$('#img-' + i).on('click', function() {
+			$('#icon' + i).on('click', function() {
 				$('#product-img-' + i).click();
 			});	
 
-			$('#pic-' + i).on('click', function() {
+			$('#pic' + i).on('click', function() {
 				$('#product-img-'  + i).click();
 			});
+		}
+	}
+
+    /** 
+	 * Change the display when the user uploads an image
+	 */
+	function changePicOnUpload() {
+		for (let i = 1; i <= maxNumPictures; i++) {
+			$('#product-img-' + i).on('change', function() {
+				readURL(this, i);
+			});
+		}
+	}
+
+    /**
+	 * Reads the URLs of the uploaded images to display them on the polaroid
+	 * 
+	 * @param input the file input field
+	 * @param i the one-based index of the file input field 
+	 */
+	function readURL(input, i) {
+		/* Get the file extension using regex */
+		let extension = input.value.match(/\.([^\.]+)$/)[1];
+		// let isImage = imgFilter(extension.toLowerCase());
+        let isImage = true;
+
+		/* The uploaded file is recognized as a valid image file type */
+		if (isImage) {
+			if (input.files && input.files[0]) {
+				let reader = new FileReader();
+				reader.onload = function(e) {
+					$('#pic' + i).css('display', 'inline-block');
+                    $('#icon' + i).css('display', 'none');
+
+                    /* Use zero-based indexing to conform with array notation */
+                    $('#pic' + i).attr('src', e.target.result)
+
+					/* Store the image file paths in an array. Subtract 1 from index since array is zero-based. */
+					imgTargetResultsOrig[i - 1] = e.target.result;
+
+                    /* Handle the case when the first photo is changed */
+                    if (i == 1) {
+                        $('#pic-big').css('display', 'block');
+                        $('#pic-big').attr('src', e.target.result);
+                        $('#icon-big').css('display', 'none');
+                    }
+
+                    /* Indicate that the photo has been modified */
+                    
+				}
+				
+				reader.readAsDataURL(input.files[0]);
+			}
+		} else {
+			/* The uploaded file is not recognized as a valid image file type */
+			input.value = '';
 		}
 	}
 });
