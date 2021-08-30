ab287411b3ab6e36468953a421e3df4eac039680
Mark Edward M. Gonzales <gonzales.markedward@gmail.com>
2021-08-31 02:28:13 +0800

    Add unit tests for validating sign-up fields (partial)

--- test/sign-up-validate-util-test.js
+++ test/sign-up-validate-util-test.js
@@ -1,5 +1,8 @@
 const assert = require('chai').assert;
-const {isAdminCredential} = require('./sign-up-validate-util');
+const {isAdminCredential,
+    isValidContactNumberText,
+    isValidZipCodeText,
+    isValidPasswordText} = require('./sign-up-validate-util');
 
 describe('the function to check if the user input is the same as the email address or username of the administrator account', function() {
     it('should return a Boolean', function() {
@@ -27,3 +30,97 @@ describe('the function to check if the user input is the same as the email addre
         assert.equal(result, false);
     });
 });
+
+describe('the function to check if a given contact number is valid', function() {
+    it('should return a Boolean', function() {
+        const result = isValidContactNumberText('#hello', '09123456789');
+        assert.isBoolean(result);
+    });
+
+    it('should return true if the contact number is 7 digits long', function() {
+        const result = isValidContactNumberText('#hello', '1234567');
+        assert.equal(result, true);
+    });
+
+    it('should return true if the contact number is 12 digits long', function() {
+        const result = isValidContactNumberText('#hello', '639123456789');
+        assert.equal(result, true);
+    });
+
+    it('should return true if the contact number is between 7 and 12 digits long', function() {
+        const result = isValidContactNumberText('#hello', '09123456789');
+        assert.equal(result, true);
+    });
+
+    it('should return false if the contact number is less than 7 digits long', function() {
+        const result = isValidContactNumberText('#hello', '403931');
+        assert.equal(result, false);
+    });
+
+    it('should return false if the contact number is more than 12 digits long', function() {
+        const result = isValidContactNumberText('#hello', '6391234567890');
+        assert.equal(result, false);
+    });
+});
+
+describe('the function to check if a given zip code is valid', function() {
+    it('should return a Boolean', function() {
+        const result = isValidZipCodeText('#hello', '1234');
+        assert.isBoolean(result);
+    });
+
+    it('should return true if the zip code is 3 digits long', function() {
+        const result = isValidZipCodeText('#hello', '700');
+        assert.equal(result, true);
+    });
+
+    it('should return true if the zip code is 4 digits long', function() {
+        const result = isValidZipCodeText('#hello', '1234');
+        assert.equal(result, true);
+    });
+
+    it('should return false if the zip code is less than 3 digits long', function() {
+        const result = isValidZipCodeText('#hello', '12');
+        assert.equal(result, false);
+    });
+
+    it('should return false if the zip code is more than 4 digits long', function() {
+        const result = isValidZipCodeText('#hello', '12345');
+        assert.equal(result, false);
+    });
+});
+
+describe('the function to check if a given password is valid', function() {
+    it('should return a Boolean', function() {
+        const result = isValidPasswordText('#hello', 'boogiepop');
+        assert.isBoolean(result);
+    });
+
+    it('should return true if the password is 8 characters long, and contains at least one letter and one number', function() {
+        const result = isValidPasswordText('#hello', 'abcdefg2');
+        assert.equal(result, true);
+    });
+
+    it('should return true if the password is more than 8 characters long, and contains at least one letter and one number', function() {
+        const result = isValidPasswordText('#hello', 'abcdefghijkl123');
+        assert.equal(result, true);
+    });
+
+    it('should return true if the password is at least 8 characters long, and contains a number before a sequence of letters', function() {
+        const result = isValidPasswordText('#hello', '1abcdefghij');
+        assert.equal(result, true);
+    });
+
+    it('should return true if the password is at least 8 characters long, and contains a letter before a sequence of numbers', function() {
+        const result = isValidPasswordText('#hello', 'a12345678');
+        assert.equal(result, true);
+    });
+
+    it('should return true if the password is at least 8 characters long, and contains a series of numbers and letters', function() {
+        const result = isValidPasswordText('#hello', 'a1b2c3d4e5');
+        assert.equal(result, true);
+    });
+
+});
+
+
