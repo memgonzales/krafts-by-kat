086270462611ee0beb369810afd2b45f33359ab9
Hylene Lee <hylene_jules_lee@dlsu.edu.ph>
2021-09-08 01:20:48 +0800

    Remove unused detail property and implement return to item catalog functionality

--- controllers/log-in-controller.js
+++ controllers/log-in-controller.js
@@ -56,7 +56,6 @@ const logInController = {
                     bcrypt.compare(password, result.password, function (err, equal) {
                         if (equal) {
                             req.session.username = businessOwner.username;
-                            req.session.isAdmin = true;
                             
                             res.status(200).json(password);
                             res.send();
@@ -101,7 +100,6 @@ const logInController = {
                     bcrypt.compare(password, result.password, function (err, equal) {
                         if (equal) {
                             req.session.username = client.username;
-                            req.session.isAdmin = false;
 
                             res.status(200).json(password);
                             res.send();
@@ -138,7 +136,6 @@ const logInController = {
                             bcrypt.compare(password, result.password, function (err, equal) {
                                 if (equal) {
                                     req.session.username = client.username;
-                                    req.session.isAdmin = false;
 
                                     res.status(200).json(password);
                                     res.send();
