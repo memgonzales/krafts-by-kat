963d75ed13a5f9e02627386b6b21154c2c6b5a43
Mark Edward M. Gonzales <gonzales.markedward@gmail.com>
2021-09-08 21:25:01 +0800

    Add partial unit testing for order item-related functions

--- test/order-specific-util-test.js
+++ test/order-specific-util-test.js
@@ -2,13 +2,40 @@ const jsdom = require('jsdom');
 const { JSDOM } = jsdom;
 
 const assert = require('chai').assert;
-const {hideProduct,
-    trackRemovedOrderItems} = require('./order-specific-util');
+const {getOrderItemId,
+    formatNumber,
+    hideProduct,
+    trackRemovedOrderItems,
+    updateTotal} = require('./order-specific-util');
+
+describe('the function to extract the ID of an order item', function(){
+    beforeEach(function() {
+        const dom = new JSDOM(
+            '<html><body><div></div></body></html>',
+            {url: 'http://localhost'});
+
+        global.window = dom.window;
+        global.document = dom.window.document;   
+        global.$ = global.jQuery = require('jquery')(window);
+    });
+
+    it('should handle the case when the container ID has two constituent tokens', function() {
+        $('div').attr('id', 'quantity-001');
+        const result = getOrderItemId(document.getElementById('quantity-001'));
+        assert.equal(result, '001');
+    });
+
+    it('should handle the case when the container ID has three constituent tokens', function() {
+        $('div').attr('id', 'item-price-001');
+        const result = getOrderItemId(document.getElementById('item-price-001'));
+        assert.equal(result, '001');
+    });
+});
 
 describe('the function to remove the selected order item from view', function() {
     beforeEach(function() {
         const dom = new JSDOM(
-            '<html><body><div id = "accordion-item-1"></div><div id = "accordion-item-2"></div></html>',
+            '<html><body><div id = "accordion-item-1"></div><div id = "accordion-item-2"></div></body></html>',
             {url: 'http://localhost'});
 
         global.window = dom.window;
@@ -30,7 +57,7 @@ describe('the function to remove the selected order item from view', function() 
 describe('the function to keep track of the order item IDs of the removed items', function() {
     beforeEach(function() {
         const dom = new JSDOM(
-            '<html><body><form><input type = "hidden" id = "removed-order-items"></form></html>',
+            '<html><body><form><input type = "hidden" id = "removed-order-items"></form></body></html>',
             {url: 'http://localhost'});
 
         global.window = dom.window;
