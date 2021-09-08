c98e9e73a3a05cbe32287c1c03e87b66552bef11
Mark Edward M. Gonzales <gonzales.markedward@gmail.com>
2021-09-08 21:13:21 +0800

    Reflect running total when order quantity is changed

--- public/js/order-specific.js
+++ public/js/order-specific.js
@@ -1,14 +1,37 @@
 $(document).ready(function() {
+    $('.order-item-price').each(function() {
+        $(this).text(formatNumber($(this).text()));
+    });
+
     $('.remove-order-item').each(function() {
         $(this).on('click', function() {
-            const id = $(this).attr('id');
-            const idTokens = id.split('-');
-            const orderItemId = idTokens[idTokens.length - 1];
+            const orderItemId = getOrderItemId(this);
 
             hideProduct(orderItemId);
             trackRemovedOrderItems(orderItemId);
         });
     });
+
+    $('.item-quantity').each(function() {
+        $(this).on('change', function() {
+            const orderItemId = getOrderItemId(this);
+            const unitPrice = $('#product-price-' + orderItemId).text();
+            const totalPrice = parseFloat(unitPrice) * parseFloat($(this).val());
+
+            $('#order-item-price-' + orderItemId).text(formatNumber(totalPrice));
+        });
+    });
+
+    function getOrderItemId(element) {
+        const id = element.id;
+        const idTokens = id.split('-');
+        
+        return idTokens[idTokens.length - 1];
+    }
+
+    function formatNumber(price) {
+        return parseFloat(price).toLocaleString('en-US', {minimumFractionDigits: 2, maximumFractionDigits: 2});
+    }
 
     function hideProduct(orderItemId) {
         $('#accordion-item-' + orderItemId).hide();
@@ -16,6 +39,11 @@ $(document).ready(function() {
 
     function trackRemovedOrderItems(orderItemId) {
         $('#removed-order-items').val($('#removed-order-items').val() + ',' + orderItemId);
+    }
+
+    function updateTotal(orderItemId) {
+        const price = $('#order-item-price-' + orderItemId).text();
+        alert(price);
     }
 
     $('#save-order').on('click', function(e) {
