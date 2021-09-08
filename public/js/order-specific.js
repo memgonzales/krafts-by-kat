5603ab622f8c4d2312f6a25c6c3866999cea2045
Hylene Lee <hylene_jules_lee@dlsu.edu.ph>
2021-09-08 14:09:09 +0800

    Add order and order item IDs in form

--- public/js/order-specific.js
+++ public/js/order-specific.js
@@ -12,4 +12,20 @@ $(document).ready(function() {
     function hideProduct(productId) {
         $('#accordion-item-' + productId).hide();
     }
+
+    /* Ajax for removing an order item from a client's order */
+	$('#remove-order-item-form').on('submit', function(e) {		
+		e.preventDefault();
+			
+		$.ajax({
+			url: '/postRemoveOrderItem',
+			method: 'POST',
+			data: $('#remove-order-item-form').serialize(),
+            statusCode: {
+				200: function(data) {
+					location.href = '/getOrder/' + data;
+				}
+			}
+		});
+	});
 })
