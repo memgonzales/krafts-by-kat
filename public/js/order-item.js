3f2d74ac829b3d1b6fc95bca8fdacd179724b669
Mark Edward M. Gonzales <gonzales.markedward@gmail.com>
2021-09-19 00:03:42 +0800

    Make order names clickable, and apply color coding to all pages

--- public/js/order-item.js
+++ public/js/order-item.js
@@ -6,15 +6,24 @@ $(document).ready(function() {
         switch(status) {
             case 'Pending':
                 $('#order-div-' + id).css('background-color', '#FDFD96');
+                $('#order-link-' + id).attr('href', '/viewSubmittedOrder/' + id);
                 break;
             case 'Accepted':
                 $('#order-div-' + id).css('background-color', '#CFF0CC');
+                $('#order-link-' + id).attr('href', '/viewSubmittedOrder/' + id);
                 break;
             case 'En Route':
                 $('#order-div-' + id).css('background-color', '#E6D1F2');
+                $('#order-link-' + id).attr('href', '/viewSubmittedOrder/' + id);
                 break;
             case 'Delivered':
                 $('#order-div-' + id).css('background-color', '#C7D9FE');
+                $('#order-link-' + id).attr('href', '/viewSubmittedOrder/' + id);
+                break;
+            case 'Unsubmitted':
+                $('#order-link-' + id).attr('href', '/getOrder/' + id);
+                break;
+            default:
                 break;
         }
     });
